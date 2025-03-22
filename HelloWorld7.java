e8870eb914249c6231f3b0ced0bf8fa7875f938c
dove7777 <102409472+dove7777@users.noreply.github.com>
2025-03-22 18:30:58 +0800

    Add files via upload

---
+++ HelloWorld7.java
@@ -0,0 +1,11 @@
+package com.itheima.demo1;
+
+public class HelloWorld7 {
+    //叫做main方法,为程序的主入口//
+    /*这是一个
+    多行注释*/
+    public static void main(String[] args) {
+         System.out.println("HelloWorld!");
+    }
+    //注释
+}
